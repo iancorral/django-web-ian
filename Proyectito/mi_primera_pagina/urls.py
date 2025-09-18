5ad783048d54f3326598124332455107393e3b4f
Ian Corral <ian.corr.mar@gmail.com>
2025-09-17 20:01:20 -0600

    add another fix

--- Proyectito/mi_primera_pagina/urls.py
+++ Proyectito/mi_primera_pagina/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("datetime/", views.current_datetime, name='current_datetime'),
+    path("greet/<str:name>/", views.greet_user, name='greet_user'),
 ]
