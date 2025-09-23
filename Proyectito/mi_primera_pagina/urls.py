3a6eac9087e866316f2e6258113852406a289a3a
Ian Corral <ian.corr.mar@gmail.com>
2025-09-22 19:31:13 -0600

    .

--- Proyectito/mi_primera_pagina/urls.py
+++ Proyectito/mi_primera_pagina/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index'),                  # Ruta principal
-    path("index1/", views.index1, name='index1'),         # Nueva ruta
-    path("hello/", views.hello, name='hello'),            # Hello World
-    path("datetime/", views.current_datetime, name='datetime'),  # Fecha y hora
-    path("greet/<str:name>/", views.greet, name='greet'), # Saludo dinámico
-    path("<str:nombre>/", views.saludo, name='saludo'), # Saludo con template
-    path("about/", views.about, name='about'),            # About page
-    path("sumar/", views.sumar, name='sumar'),            # Sumar dos números
+    path("", views.index, name='index'),
+    path("index1/", views.index1, name='index1'),
+    path("hello/", views.hello, name='hello'),
+    path("datetime/", views.current_datetime, name='datetime'),
+    path("greet/<str:name>/", views.greet, name='greet'),
+    path("about/", views.about, name='about'),
+    path("sumar/", views.sumar, name='sumar'),
+    path("<str:nombre>/", views.saludo, name='saludo'),  
 ]
